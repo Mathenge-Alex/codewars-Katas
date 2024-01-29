be7e4027c8f3088baf7611f9a8039a8d0cfe190b
Mathenge-Alex <arxmaina@gmail.com>
2024-01-30 01:08:23 +0300

    Update kata solution

--- Calculating_with_functions.py
+++ Calculating_with_functions.py
@@ -13,3 +13,20 @@
 # Division should be integer division. For example, this should return 2, not 2.666666...:
 # eight(divided_by(three()))
 
+# Solution
+
+def zero(func=None): return 0 if not func else func(0)
+def one(func=None): return 1 if not func else func(1)
+def two(func=None): return 2 if not func else func(2)
+def three(func=None): return 3 if not func else func(3)
+def four(func=None): return 4 if not func else func(4)
+def five(func=None): return 5 if not func else func(5)
+def six(func=None): return 6 if not func else func(6)
+def seven(func=None): return 7 if not func else func(7)
+def eight(func=None): return 8 if not func else func(8)
+def nine(func=None): return 9 if not func else func(9)
+
+def plus(a): return lambda x:x + a
+def minus(a): return lambda x:x - a
+def times(a): return lambda x:x * a
+def divided_by(a): return lambda x:x // a
